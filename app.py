2c85f245c8a6418431278fbc238db17f207f4185
tchomasek <tchom.asek@centrum.cz>
2020-10-17 11:50:43 +0200

    added run.py, small error correction

--- app.py
+++ app.py
@@ -22,4 +22,7 @@ api.add_resource(UserRegister,'/register')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 if __name__=='__main__':
+    from db import db
+
+    db.init_app(app)
     app.run(port=5000,debug=True)
